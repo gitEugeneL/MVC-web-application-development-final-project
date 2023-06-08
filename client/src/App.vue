d716bc7e84016cbf151014fa7efcb4cfc1bfae40
gitEugeneL <l.eugene.arch@gmail.com>
2023-06-08 14:51:07 +0200

    init vue client

---
+++ client/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="app">
+
+        <!--    <NavBar />-->
+
+        <div class="main-wrapper">
+            hello
+            <!--      <router-view />-->
+        </div>
+    </div>
+</template>
